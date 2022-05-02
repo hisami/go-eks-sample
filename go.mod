c12e4f4214630a9b1a64ce2ab5f86d5c68cb7608
hisami <ryohei.hisamitsu@ncdc.co.jp>
2022-05-02 21:16:18 +0900

    ginの導入

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module rekognition-go
 
 go 1.16
 
-require github.com/aws/aws-sdk-go v1.44.4
+require (
+	github.com/aws/aws-sdk-go v1.44.4
+	github.com/gin-gonic/gin v1.7.7
+)
--- main.go
+++ main.go
@@ -8,56 +8,52 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/rekognition"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	// // 引数から画像ファイルのURLを取得
-	// f := flag.String("url", "http://examplea.com/", "URL")
-	// flag.Parse()
+	router := gin.Default()
 
-	// // 画像ファイルを取得
-	// image, err := http.Get(*f)
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// 	return
-	// }
+	router.GET("/", func(c *gin.Context) {
+		file, err := os.Open("img/montes.png")
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
 
-	file, err := os.Open("img/montes.png")
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
+		// 最後にファイルを閉じる
+		defer file.Close()
 
-	// 最後にファイルを閉じる
-	defer file.Close()
+		// 画像ファイルのデータを全て読み込み
+		bytes, err := ioutil.ReadAll(file)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
 
-	// 画像ファイルのデータを全て読み込み
-	bytes, err := ioutil.ReadAll(file)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
+		// セッション開始
+		sess := session.Must(session.NewSession())
 
-	// セッション開始
-	sess := session.Must(session.NewSession())
+		// Rekognitionクライアントを作成
+		svc := rekognition.New(sess, aws.NewConfig().WithRegion("ap-northeast-1"))
 
-	// Rekognitionクライアントを作成
-	svc := rekognition.New(sess, aws.NewConfig().WithRegion("ap-northeast-1"))
+		// DetectFacesに渡すパラメータを設定
+		params := &rekognition.DetectTextInput{
+			Image: &rekognition.Image{
+				Bytes: bytes,
+			},
+		}
 
-	// DetectFacesに渡すパラメータを設定
-	params := &rekognition.DetectTextInput{
-		Image: &rekognition.Image{
-			Bytes: bytes,
-		},
-	}
+		// DetectFacesを実行
+		resp, err := svc.DetectText(params)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
 
-	// DetectFacesを実行
-	resp, err := svc.DetectText(params)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
+		// 結果を出力
+		fmt.Println(resp)
+	})
 
-	// 結果を出力
-	fmt.Println(resp)
+	router.Run()
 }
