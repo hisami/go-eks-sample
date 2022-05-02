b27049ce21a3e71f4d982255c104bf8c0b33a3bc
hisami <ryohei.hisamitsu@ncdc.co.jp>
2022-05-02 21:07:23 +0900

    画像からテキストを抽出する

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"io/ioutil"
-	"net/http"
+	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -12,19 +11,28 @@ import (
 )
 
 func main() {
-	// 引数から画像ファイルのURLを取得
-	f := flag.String("url", "http://examplea.com/", "URL")
-	flag.Parse()
+	// // 引数から画像ファイルのURLを取得
+	// f := flag.String("url", "http://examplea.com/", "URL")
+	// flag.Parse()
 
-	// 画像ファイルを取得
-	image, err := http.Get(*f)
+	// // 画像ファイルを取得
+	// image, err := http.Get(*f)
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// 	return
+	// }
+
+	file, err := os.Open("img/montes.png")
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
 
+	// 最後にファイルを閉じる
+	defer file.Close()
+
 	// 画像ファイルのデータを全て読み込み
-	bytes, err := ioutil.ReadAll(image.Body)
+	bytes, err := ioutil.ReadAll(file)
 	if err != nil {
 		fmt.Println(err.Error())
 		return
@@ -37,17 +45,14 @@ func main() {
 	svc := rekognition.New(sess, aws.NewConfig().WithRegion("ap-northeast-1"))
 
 	// DetectFacesに渡すパラメータを設定
-	params := &rekognition.DetectFacesInput{
+	params := &rekognition.DetectTextInput{
 		Image: &rekognition.Image{
 			Bytes: bytes,
-		},
-		Attributes: []*string{
-			aws.String("ALL"),
 		},
 	}
 
 	// DetectFacesを実行
-	resp, err := svc.DetectFaces(params)
+	resp, err := svc.DetectText(params)
 	if err != nil {
 		fmt.Println(err.Error())
 		return
