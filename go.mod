16b61b307814aee3c1dbc68021e149a08687c72f
hisami <ryohei.hisamitsu@ncdc.co.jp>
2022-10-21 09:16:28 +0900

    DBの接続テストができるところまで

--- go.mod
+++ go.mod
@@ -5,4 +5,7 @@ go 1.16
 require (
 	github.com/aws/aws-sdk-go v1.44.4
 	github.com/gin-gonic/gin v1.7.7
+	github.com/joho/godotenv v1.4.0
+	gorm.io/driver/postgres v1.4.5
+	gorm.io/gorm v1.24.1-0.20221019064659-5dd2bb482755
 )
--- main.go
+++ main.go
@@ -3,18 +3,42 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/rekognition"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
 	router := gin.Default()
+	// 環境変数の読み込み
+	godotenv.Load(".env")
 
 	router.GET("/", func(c *gin.Context) {
 		c.String(200, "Server Running!!")
+	})
+
+	router.GET("/db-connect-test", func(c *gin.Context) {
+		dbHost := os.Getenv("DB_HOST")
+		dbUser := os.Getenv("DB_USER")
+		dbPassword := os.Getenv("DB_PASSWORD")
+		dbName := os.Getenv("DB_NAME")
+
+		dsn := "host=" + dbHost + " user=" + dbUser + " password=" + dbPassword + " dbname=" + dbName
+		_, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+		if err != nil {
+			fmt.Println(err.Error())
+			c.JSON(500, gin.H{"msg": "internal server error"})
+			return
+		}
+
+		c.String(200, "DB connect success!!")
 	})
 
 	router.POST("/", func(c *gin.Context) {
